c531b1ee0a9dfe60de4636302c9fe8fa99120942
ashishtam <ashishtamrakar@gmail.com>
2013-04-02 09:07:42 +0545

    autoloader function added in bootstrap.php

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -33,5 +33,17 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $router->addConfig($config,'routes');
     }
     
+    protected function _initAutoloader()
+    {
+        $loader = new Zend_Application_Module_Autoloader(
+                    array(
+                        "namespace" => "",
+                        "basePath"=> APPLICATION_PATH."/modules/default")
+                );
+        
+        return $loader;
+    
+    }
+    
 }
 
